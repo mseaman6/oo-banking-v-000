f6d7884ec9953ebcec9be5c4199c048797c3d29b
Monica Seaman <monica.l.seaman@gmail.com>
2018-05-23 19:00:50 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,30 @@
 class BankAccount
 
+  attr_reader :name, :status, :balance
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(deposit_amount)
+    self.balance += deposit_amount
+  end
+
+  def display_balance
+    self.balance
+  end
+
+  def valid?
+    if self.status == "open" && self.balance > 0
+      true
+    else
+      false
+    end
+  end
+
+  def close_account
+    self.status = "closed"
+
 end
