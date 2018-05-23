6bc7bbc26364afe5aa4911874dd99aa547eac9ab
Monica Seaman <monica.l.seaman@gmail.com>
2018-05-23 19:15:36 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,28 @@
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver
+  attr_reader :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    if self.sender.valid? && self.receiver.valid?
+      true
+    else
+      false
+    end
+  end
+
+  def execute_transaction
+    if self.valid?
+      self.sender.deposit(-amount)
+    else
+      "The transaction has been rejected."
+  end
+
 end
